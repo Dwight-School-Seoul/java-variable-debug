d9610860c9ee257e0c15d27b22667dcb4ba8940a
lichi <lichi.liqi@gmail.com>
2023-01-11 20:09:58 +0900

    debug file

---
+++ src/DateClass.java
@@ -0,0 +1,16 @@
+public class DateClass {
+
+    public static int weekDay = "Friday";
+
+    public static double month = "January";
+
+    public static String date = 13;
+
+    public static boolean year = 2023;
+
+    public static void getDate(){
+
+        System.out.println("today is "+ year + aDash + month + aDash + date+", it is "+weekDay );
+    }
+
+}
